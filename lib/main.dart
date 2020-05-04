440556a3dc8365dca586d7d71f1beb6692e89de3
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2020-05-04 01:58:54 -0400

    Set background color

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.teal[50],
         body: Container(),
       ),
     );
