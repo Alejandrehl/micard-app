0d3cdeb01a24337d2c3daea12433c2358489103e
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2020-05-04 03:12:15 -0400

    Adding images

--- lib/main.dart
+++ lib/main.dart
@@ -12,40 +12,11 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
             children: <Widget>[
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.red,
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        height: 100.0,
-                        width: 100.0,
-                        color: Colors.yellow,
-                      ),
-                      Container(
-                        height: 100.0,
-                        width: 100.0,
-                        color: Colors.green,
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.blue,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red[50],
               ),
             ],
           ),
